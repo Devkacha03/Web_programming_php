fb557021c41dd45b6d9a5450b363217894546291
dev kacha <devkacha90@gmail.com>
2024-12-10 21:26:01 +0530

    add file handle related code

--- cia/filehandle.php
+++ cia/filehandle.php
@@ -1,21 +1,54 @@
 <?php
+// $file_name = "dev.txt";
+// $my_file = fopen($file_name, "w+");
+// if ($my_file) {
+//     echo "<script>alert('file open');</script>";
+
+//     fwrite($my_file, "dev kacha");
+
+// rewind($my_file);
+// while (($line = fgets($my_file)) !== false) {
+//     echo nl2br($line);
+// }
+
+// rewind($my_file);
+// $r_file = fread($my_file, filesize("dev.txt"));
+// echo nl2br($r_file);
+
+//     fclose($my_file);
+// } else {
+//     echo "<script>alert('file unable to open');</script>";
+// }
+
+
+//! COPY FILE ERROR
+
+// $file_name = "dev.txt";
+// $source_file = "co.txt";
+
+// if (copy($file_name, $source_file)) {
+//     echo "copie.";
+// } else {
+//     echo "error";
+// }
+
+//! move or rename file
+
 $file_name = "dev.txt";
-$my_file = fopen($file_name, "w+");
-if ($my_file) {
-    echo "<script>alert('file open');</script>";
+$source_file = "co.txt";
 
-    fwrite($my_file, "dev kacha");
+//* move file
 
-    // rewind($my_file);
-    // while (($line = fgets($my_file)) !== false) {
-    //     echo nl2br($line);
-    // }
+// if (rename($file_name, $source_file)) {
+//     echo "success full moved";
+// } else {
+//     echo "an error";
+// }
 
-    // rewind($my_file);
-    // $r_file = fread($my_file, filesize("dev.txt"));
-    // echo nl2br($r_file);
+//* rename file
 
-    fclose($my_file);
-} else {
-    echo "<script>alert('file unable to open');</script>";
-}
+// if (rename($source_file, $file_name)) {
+//     echo "success full moved";
+// } else {
+//     echo "an error";
+// }
